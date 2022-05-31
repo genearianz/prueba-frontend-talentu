f274f59ecae18718bc6508ca9ed61f640376bb4f
gabada01 <gabada01@debian-BULLSEYE-live-builder-AMD64>
2022-05-31 08:51:34 -0400

    prueba frontend talentu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="bg-primary d-flex justify-content-center align-items-center">
+    <h5 class="text-white"><b>Frontend Talentu</b></h5>
+  </header>
   <router-view/>
 </template>
 
@@ -15,16 +14,12 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+header {
+  height: 60px;
+  box-shadow: 0px 3px 15px 5px rgba(#0d6efd, 0.2);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h5 {
+  margin-bottom: 0px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,177 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container mt-5">
+
+        <div class="alert alert-dismissible fade show" :class="alertClass" role="alert" v-if="alert">
+            {{ this.alertMessage }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" v-on:click="closeAlert"></button>
+        </div>
+
+        <div class="row">
+
+            <!-- REGISTRAR UN USUARIO NUEVO -->
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        Agregar Usuario
+                    </div>
+                    <div class="card-body">
+                        
+                        <form v-on:submit="addNewUser">
+                            <div class="mb-3 text-start">
+                                <label class="form-label">Nombre</label>
+                                <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    v-model="user.first_name" 
+                                    placeholder="Nombre">
+                            </div>
+                            <div class="mb-3 text-start">
+                                <label class="form-label">Apellido</label>
+                                <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    v-model="user.last_name" 
+                                    placeholder="Apellido">
+                            </div>
+                            <div class="mb-3 text-start">
+                                <label class="form-label">Correo</label>
+                                <input 
+                                    type="email" 
+                                    class="form-control" 
+                                    v-model="user.email" 
+                                    placeholder="Correo">
+                            </div>
+                            <div class="mb-3 text-start">
+                                <label class="form-label">Fecha de Nacimiento</label>
+                                <input 
+                                    type="date" 
+                                    class="form-control" 
+                                    v-model="user.birthday" 
+                                    placeholder="Fecha de Nacimiento">
+                            </div>
+                            
+                            <button v-on:click="addNewUser" class="btn btn-primary w-100">Agregar Usuario</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- MOSTRAR USUARIOS -->
+            <div class="col-md-9 mb-3">
+                <div class="card">
+                    <div class="card-body table-responsive">
+                        <table class="table">
+                            <thead class="table-dark">
+                                <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Edad</th>
+                                <th scope="col">Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="user, index in users" v-bind:key="index">
+                                <td>{{ user.id }}</td>
+                                <td>{{ user.first_name }} {{ user.last_name }}</td>
+                                <td>{{ user.birthday }}</td>
+                                <td>{{ user.email }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
+
+export default {  
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      users: null,      
+      user: {
+          first_name: '',
+          last_name: '',
+          email: '',
+          birthday: ''
+      },
+      alert: false,
+      name: '',
+      alertClass: '',
+      alertMessage: ''
+    }
+  },
+  created(){
+
+        let dataLocal = localStorage.getItem('users');
+
+        if(dataLocal != null ){
+            this.users = JSON.parse(dataLocal); 
+        } else {
+            this.getUsers();
+        }
+
+  },
+  methods: {
+    getUsers() {
+        axios.get('https://reqres.in/api/users?page=1')
+            .then( res => {
+                    this.users = res.data.data;
+                    })
+            .catch( e => console.log(e));
+
+            console.log('asd')
+    },
+    addNewUser(e){
+        e.preventDefault();
+        let id = this.users.length+1;
+        let date = new Date().getFullYear();
+        let birthday = new Date(this.user.birthday).getFullYear();
+        let age = date - birthday;
+
+        if(this.user.first_name && this.user.last_name && this.user.email && this.user.birthday) {
+
+            this.users.push({
+                id: id,
+                first_name: this.user.first_name,
+                last_name: this.user.last_name,
+                birthday: age,
+                email: this.user.email,
+            });
+            
+            this.alertClass = "alert-success";
+            this.alertMessage = "Se agrego el usuario: " + this.user.first_name + " " + this.user.last_name + " correctamente!";
+            this.alert = true;
+            this.name = this.user.first_name + " " + this.user.last_name;
+            this.user = {
+                first_name: "",
+                last_name: "",
+                birthday: "",
+                email: "",
+            }
+
+            this.localStorage();
+
+        } else {
+            this.alertClass = "alert-danger";
+            this.alertMessage = "Error al agregar usuario. Por favor intente nuevamente y asegurese de rellenar los campos."
+            this.alert = true;   
+        }
+    },
+    localStorage(){
+        localStorage.setItem('users', JSON.stringify(this.users));
+    },
+    closeAlert(){
+        return this.alert = false;
+    }
+  },
+  mounted(){
+    //this.getUsers();
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
